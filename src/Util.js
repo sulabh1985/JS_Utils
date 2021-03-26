89b0022af32d5d9f5354248265194d7afbc7ffb2
Sulabh Agarwal <sulabh1985@gmail.com>
2021-03-26 12:39:25 +0530

    Added Show/Hide HTML element by Id.

--- src/Util.js
+++ src/Util.js
@@ -1,15 +1,25 @@
 function Utility(){
 	var _showElementById = function(elementId){
-		document.getElementById(elementId).style.display='none';
+		document.getElementById(elementId).style.display='';
 	};
 	var _showElementByIds = function(elementIds){
 		numbers.forEach(function(element) {
 			this._showElementById(element);
 		});
 	};
+	var _hideElementById = function(elementId){
+		document.getElementById(elementId).style.display='none';
+	};
+	var _hideElementByIds = function(elementIds){
+		numbers.forEach(function(element) {
+			this._hideElementById(element);
+		});
+	};	
 	return {
-		showElementById:_showElementById
-		showElementByIds:_showElementByIds
+		showElementById:_showElementById,
+		showElementByIds:_showElementByIds,
+		hideElementById:_hideElementById,
+		hideElementByIds:_hideElementByIds		
 	};
 }
 

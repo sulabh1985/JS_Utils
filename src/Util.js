08a5a611ba6039cf58097855d0f760671f5aa213
Sulabh Agarwal <sulabh1985@gmail.com>
2021-03-26 13:51:16 +0530

    Added Show/Hide HTML element by Multiple Ids.

--- src/Util.js
+++ src/Util.js
@@ -3,16 +3,16 @@ function Utility(){
 		document.getElementById(elementId).style.display='';
 	};
 	var _showElementByIds = function(elementIds){
-		numbers.forEach(function(element) {
-			this._showElementById(element);
+		elementIds.forEach(function(element) {
+			_showElementById(element);
 		});
 	};
 	var _hideElementById = function(elementId){
 		document.getElementById(elementId).style.display='none';
 	};
 	var _hideElementByIds = function(elementIds){
-		numbers.forEach(function(element) {
-			this._hideElementById(element);
+		elementIds.forEach(function(element) {
+			_hideElementById(element);
 		});
 	};	
 	return {
